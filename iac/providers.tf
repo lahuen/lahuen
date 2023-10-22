0ac4bbfc7dabe497b2c43e04990d549897f06c26
Max Florentin <86262167+maxflorentin@users.noreply.github.com>
2023-10-22 19:31:48 -0300

    Update providers.tf
    change aws provider version

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,4 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-  version = "~> 3.79"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
 }
