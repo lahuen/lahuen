f3267b3a6562aa65b4cd8cc735b156f5b3d0d10b
Max Florentin <86262167+maxflorentin@users.noreply.github.com>
2023-10-24 22:32:05 -0300

    Update outputs.tf

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,3 +1,11 @@
 output "lahuen_dl_bucket_landing" {
-  value = aws_s3_bucket.lahuen-dl-landing.arn
+  value = aws_s3_bucket.lahuen-dl-landing.bucket
+}
+
+output "lahuen_dl_bucket_raw" {
+  value = aws_s3_bucket.lahuen-dl-raw.bucket
+}
+
+output "lahuen_dl_bucket_core" {
+  value = aws_s3_bucket.lahuen-dl-core.bucket
 }
