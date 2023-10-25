3f41f885595b519766ed47b444d32bef2960a1c9
Max Florentin <86262167+maxflorentin@users.noreply.github.com>
2023-10-25 14:55:52 -0300

    Update s3.tf

--- iac/s3.tf
+++ iac/s3.tf
@@ -15,28 +15,6 @@ resource "aws_s3_bucket_public_access_block" "landing" {
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_bucket_versioning" "landing" {
-#   bucket = aws_s3_bucket.lahuen-dl-landing.id
-# }
-
-resource "aws_s3_bucket" "lahuen-dl-raw" {
-  bucket = "lahuen-dl-raw"
-  acl    = "private"
-
-  versioning {
-    enabled = false
-  }
-}
-
-resource "aws_s3_bucket" "lahuen-dl-core" {
-  bucket = "lahuen-dl-core"
-  acl    = "private"
-
-  versioning {
-    enabled = false
-  }
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "landing" {
   rule {
     apply_server_side_encryption_by_default {
@@ -47,6 +25,27 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "landing" {
   bucket = aws_s3_bucket.lahuen-dl-landing.id
 }
 
+# resource "aws_s3_bucket_versioning" "landing" {
+#   bucket = aws_s3_bucket.lahuen-dl-landing.id
+# }
+
+resource "aws_s3_bucket" "lahuen-dl-raw" {
+  bucket = "lahuen-dl-raw"
+}
+
+resource "aws_s3_bucket_acl" "raw" {
+  bucket = aws_s3_bucket.lahuen-dl-raw.id
+}
+
+resource "aws_s3_bucket_public_access_block" "raw" {
+  bucket = aws_s3_bucket.lahuen-dl-raw.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "raw" {
   rule {
     apply_server_side_encryption_by_default {
@@ -55,6 +54,19 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "raw" {
   }
 
   bucket = aws_s3_bucket.lahuen-dl-raw.id
+}
+
+# resource "aws_s3_bucket_versioning" "raw" {
+#   bucket = aws_s3_bucket.lahuen-dl-raw.id
+# }
+
+resource "aws_s3_bucket" "lahuen-dl-core" {
+  bucket = "lahuen-dl-core"
+  acl    = "private"
+
+  versioning {
+    enabled = false
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "core" {
