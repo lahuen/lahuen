06a404f2099c1048def2aecf1bd3a01a72e0a168
Max Florentin <86262167+maxflorentin@users.noreply.github.com>
2023-11-03 15:15:11 -0300

    Delete iac directory

--- iac/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lahuen-tf"
-    key            = "data/lahuen.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "lahuen-tf"
-  }
-}
